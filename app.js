d6c0c2038898001dcd2eba83069906c73bcd0bda
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-23 19:26:40 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express=require('express');
 require('dotenv').config();
-
 const app=express();
+
+
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+res.send('get all tasks')
+})
+
+
 
 const port=8080;
 
