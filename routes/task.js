ab5fe3f00919c20bb792306fa3d041a49a9b6e61
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-23 20:36:20 +0330

    Update routes/task.js

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-
+const { getAllTask,createTask, getTask,updateTask,deleteTask}=require('../controller/task');
 
 router.route('/');
 router.route('/:id');
