710425f47cac3ce7eef766712bc425042308d5f0
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-23 20:48:15 +0330

    Update routes/task.js adedd (get,post,put,del)

--- routes/task.js
+++ routes/task.js
@@ -2,8 +2,8 @@ const express=require('express');
 const router=express.Router();
 const { getAllTask,createTask, getTask,updateTask,deleteTask}=require('../controller/task');
 
-router.route('/');
-router.route('/:id');
+router.route('/').get(getAllTask).post(createTask);
+router.route('/:id').get(getTask).put(updateTask).delete(deleteTask);
 
 
 
