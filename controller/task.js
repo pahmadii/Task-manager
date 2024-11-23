dfdbd43c91e65bd5c376d50548141a8ac9c09ae0
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-23 20:24:04 +0330

    Create Controller added task.js

---
+++ controller/task.js
@@ -0,0 +1,27 @@
+const getAllTask=(req,res)=>{
+res.send('get all task')
+};
+
+const createTask=(req,res)=>{
+ res.send('create task')
+ };
+
+ const getTask=(req,res)=>{
+ res.send('get single task')
+};
+
+ const updateTask=(req,res)=>{
+res.send('update task')
+ };
+
+const deleteTask=(req,res)=>{
+ res.send('delete task')  
+};
+
+   module.exports={
+    getAllTask,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+   }                                 
