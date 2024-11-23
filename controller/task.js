702a30eda43017ef7e229cd6f5aa7ba383da2ceb
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-23 21:27:46 +0330

    1st/test API postmen /Update controller/task.js

--- controller/task.js
+++ controller/task.js
@@ -3,11 +3,11 @@ res.send('get tasks')
 };
 
 const createTask=(req,res)=>{
- res.send('create task')
+ res.json(req.body)
  };
 
  const getTask=(req,res)=>{
- res.send('get single task')
+ res.json({id:req.params.id})
 };
 
  const updateTask=(req,res)=>{
@@ -15,7 +15,7 @@ res.send('update task')
  };
 
 const deleteTask=(req,res)=>{
- res.send('delete task')  
+ res.send('delete9 task')  
 };
 
    module.exports={
