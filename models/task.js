a03b8b5d451cfe923b3919ebdfc048707915f3ea
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-24 17:22:06 +0330

    1st/postman test/2sec/update Model task.js

--- models/task.js
+++ models/task.js
@@ -2,8 +2,17 @@ const mongoose=require('mongoose');
 
 const TaskSchema=new mongoose.Schema({
 
-    name:String,
-    completed:Boolean,
+    name:{
+        type:String,
+        required:[true,'most provide name'],
+        trim:true,
+        maxlength:[20,'name can not be more than 20 charachter']
+    },
+
+    completed:{
+        type:Boolean,
+        default:false,
+    },
 });
 
 module.exports=mongoose.model('Task',TaskSchema);
