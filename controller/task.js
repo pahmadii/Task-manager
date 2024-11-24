5b8a4e40cc6789f5ae40f0f7ae0b103ea5ca2a63
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-24 17:10:25 +0330

    update controller/post/create

--- controller/task.js
+++ controller/task.js
@@ -4,8 +4,9 @@ const getAllTask=(req,res)=>{
 res.send('get tasks')
 };
 
-const createTask=(req,res)=>{
- res.json(req.body)
+const createTask=async(req,res)=>{
+   const task=await Task.create(req.body)
+ res.status(201).json({task})
  };
 
  const getTask=(req,res)=>{
