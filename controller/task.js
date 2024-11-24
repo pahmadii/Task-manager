b78adc074329bc9188ff7123b1fd69cafad91e0b
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-24 18:46:55 +0330

    update controller/complete patch api

--- controller/task.js
+++ controller/task.js
@@ -35,9 +35,29 @@ res.status(500).json({msg:error})
  
 };
 
- const updateTask=(req,res)=>{
-res.send('update task')
+ const updateTask=async(req,res)=>{
+try {
+
+   const{id:taskID}=req.params;
+const task=await Task.findOneAndUpdate({_id:taskID},req.body,{
+   new:true,runValidators:true,
+});
+
+if(!task){
+   return res.status(404).json({msg:`no task with id:${taskID}`})
+}
+
+
+   res.status(200).json({task});
+
+} catch (error) {
+   res.status(500).json({msg:error})
+}
+
  };
+
+
+
 
 const deleteTask=async(req,res)=>{
    try {
