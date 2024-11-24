38b4898d61bb150843aa020aca2ab2b9990d2969
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-24 18:14:11 +0330

    update controller/ Copmplete delete api

--- controller/task.js
+++ controller/task.js
@@ -39,9 +39,21 @@ res.status(500).json({msg:error})
 res.send('update task')
  };
 
-const deleteTask=(req,res)=>{
- res.send('delete9 task')  
+const deleteTask=async(req,res)=>{
+   try {
+      const{id:taskID}=req.params;
+      const task=await Task.findOneAndDelete({_id:taskID});
+      if(!task){
+         return res.status(404).json({msg:`no task with id:${taskID}`})
+      }
+res.status(200).json({task});
+
+   } catch (error) {
+      res.status(500).json({msg:error})
+  
 };
+}
+   
 
    module.exports={
     getAllTask,
