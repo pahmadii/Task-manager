c35b2d8a3130bf432d5c56c95fa97b677253a716
ahmadi71 <fairyahmadi71@gmail.com>
2024-11-25 15:40:05 +0330

    routes / task.js/ remove put api

--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const router=express.Router();
-const { getAllTask,createTask, getTask,updateTask,deleteTask,editTask}=require('../controller/task');
+const { getAllTask,createTask, getTask,updateTask,deleteTask}=require('../controller/task');
 
 router.route('/').get(getAllTask).post(createTask);
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask).put(editTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 
 
